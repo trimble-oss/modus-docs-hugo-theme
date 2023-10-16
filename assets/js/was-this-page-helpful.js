5033fc5875e647db2b583192ca2c1aed229fcb6d
Christian Oliff <christianoliff@pm.me>
2023-10-16 16:31:00 +0900

    Fixes

--- assets/js/was-this-page-helpful.js
+++ assets/js/was-this-page-helpful.js
@@ -1,15 +1,22 @@
 $(function () {
-  $('.feedback').fadeIn('slow').addClass('d-block').removeClass('d-none');
+  $('.feedback').fadeIn('slow').addClass('d-block');
   $('.btn-feedback').click(function () {
     $('.btn-feedback').addClass('img-grayscale').attr('disabled', 'disabled');
     $('.feedback-thanks').fadeIn('slow').addClass('d-block');
   });
+
   $('.btn-feedback-helpful').click(function () {
     $('.feedback-helpful').fadeIn('slow').addClass('d-block');
-    $('.feedback-helpful').removeClass('d-none');
+    $('.icon-helpful-solid').addClass('d-block');
+    $('.icon-helpful-solid').removeClass('d-none');
+    $('.icon-helpful-outlined').removeClass('d-block');
+    $('.icon-helpful-outlined').addClass('d-none');
   });
   $('.btn-feedback-not-helpful').click(function () {
     $('.feedback-not-helpful').fadeIn('slow').addClass('d-block');
-    $('.feedback-not-helpful').removeClass('d-none');
+    $('.icon-not-helpful-solid').addClass('d-block');
+    $('.icon-not-helpful-solid').removeClass('d-none');
+    $('.icon-not-helpful-solid').removeClass('d-none');
+    $('.icon-not-helpful-outlined').addClass('d-none');
   });
 });
