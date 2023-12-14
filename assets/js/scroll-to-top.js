c88b74c6ef4ce1c90b41deb62f8eec9f020eb89b
coliff <christianoliff@pm.me>
2023-12-14 16:57:05 +0900

    update deps

--- assets/js/scroll-to-top.js
+++ assets/js/scroll-to-top.js
@@ -4,30 +4,30 @@
  * Licensed under MIT
  */
 
-let target = document.querySelector('footer');
+const ScrollToTop = (() => {
+  const target = document.querySelector('footer');
+  const scrollToTopBtn = document.querySelector('.btn-to-top');
+  const rootElement = document.documentElement;
 
-let scrollToTopBtn = document.querySelector('.btn-to-top');
-let rootElement = document.documentElement;
-
-function callback(entries, observer) {
-  entries.forEach((entry) => {
-    window.onscroll = function (e) {
+  function callback(entries, observer) {
+    entries.forEach((entry) => {
       if (entry.isIntersecting) {
         scrollToTopBtn.classList.add('btn-to-top-show');
       } else {
         scrollToTopBtn.classList.remove('btn-to-top-show');
       }
-    };
-  });
-}
+    });
+  }
 
-function scrollToTop() {
-  rootElement.scrollTo({
-    top: 0,
-    behavior: 'smooth',
-  });
-}
-scrollToTopBtn.addEventListener('click', scrollToTop);
+  function scrollToTop() {
+    rootElement.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  }
 
-const observer = new IntersectionObserver(callback);
-observer.observe(target);
+  scrollToTopBtn.addEventListener('click', scrollToTop);
+
+  const observer = new IntersectionObserver(callback);
+  observer.observe(target);
+})();
